5437f287aabf378d53f645413a60082364fe0ad3
HerasymovIvan <gerasymov.ivan@gmail.com>
2024-09-25 18:41:19 +0300

    class item

---
+++ Inherance2/item.cpp
@@ -0,0 +1,49 @@
+#include "item.h"
+
+Item::Item()
+{
+}
+
+Item::Item(string title, string author, int year, float price)
+{
+}
+
+string Item::getTitle() const
+{
+    return string();
+}
+
+void Item::setTitle(string Title)
+{
+}
+
+string Item::getAuthor() const
+{
+    return string();
+}
+
+void Item::setAuthor()
+{
+}
+
+int Item::getyear() const
+{
+    return 0;
+}
+
+void Item::setYear(int year)
+{
+}
+
+float Item::getPrice() const
+{
+    return 0.0f;
+}
+
+void Item::setPrice(float Price)
+{
+}
+
+void Item::showInfo() const
+{
+}
